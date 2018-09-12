8e676650f8faae67768530eb16dddcfc3e813282
Ravinandan Patel <ravinandan@edcast.com>
2018-09-12 18:10:56 +0530

    Initial Commit

---
+++ MySDK/MySDK.h
@@ -0,0 +1,19 @@
+//
+//  MySDK.h
+//  MySDK
+//
+//  Created by RAVI on 12/09/18.
+//  Copyright © 2018 ravi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySDK.
+FOUNDATION_EXPORT double MySDKVersionNumber;
+
+//! Project version string for MySDK.
+FOUNDATION_EXPORT const unsigned char MySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySDK/PublicHeader.h>
+
+

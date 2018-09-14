ab1a26c46cd4c1f66fd16b30ad89e5496e66c0bd
Ravinandan Patel <ravinandan@edcast.com>
2018-09-14 16:23:31 +0530

    add class

---
+++ MySDK/MySDK.swift
@@ -0,0 +1,15 @@
+//
+//  MySDK.swift
+//  MySDK
+//
+//  Created by RAVI on 12/09/18.
+//  Copyright © 2018 ravi. All rights reserved.
+//
+
+import Foundation
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
